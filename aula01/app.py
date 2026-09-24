a91664faf6761a590f75998bbf577d2419f04b8b
Kaique-Richard-Hames <hameskaique2@gmail.com>
2026-09-23 21:48:18 -0300

    Leitura de dados e infusão das métricas(preco_medio e contar_5_estrelas)

--- aula01/app.py
+++ aula01/app.py
@@ -2,6 +2,20 @@
 """
 
 import streamlit as st
+import dados
 
+
+st.set_page_config(layout="wide")
 st.title("📚 Dashboard de Livros")
-st.write("Se você está vendo esta página, o seu ambiente está pronto! 🎉")
+
+livros = dados.ler_livros()
+qnt_livros = len(livros)
+preco_medio = dados.calcular_preco_medio(livros)
+qnt_cinco_estrelas = dados.contar_cinco_estrelas(livros)
+
+col1, col2, col3 = st.columns(3)
+col1.metric("Total de livros", qnt_livros)
+col2.metric("Preço médio dos livros", f"£{round(preco_medio, 2)}")
+col3.metric("Quantidade de livros com 5 estrelas", qnt_cinco_estrelas)
+
+st.dataframe(livros)
--- aula01/dados.py
+++ aula01/dados.py
@@ -1,9 +1,46 @@
 """Leitura dos arquivos CSV do projeto.
 """
 
+import csv
 from pathlib import Path
+from sys import exception
 
 # Pasta onde este arquivo .py está. Assim o programa encontra o CSV
 # mesmo quando é executado a partir de outra pasta (como no Streamlit Cloud).
 PASTA = Path(__file__).parent
 CAMINHO_LIVROS = PASTA / "livros.csv"
+
+def ler_livros():
+    livros = []
+    try:
+        with open(CAMINHO_LIVROS, "r", encoding="utf-8") as arquivo:
+            leitor = csv.DictReader(arquivo)
+            for linha in leitor:
+                livros.append(linha)
+    except FileNotFoundError:
+        print("Arquivo não encontrado.")
+    except exception as error:
+        print("Erro ao ler o arquivo.", error)
+
+    return livros
+
+def calcular_preco_medio(livros):
+    total = 0
+    for livro in livros:
+        num_limpo: str = livro["preco"].replace("£","")
+        total += float(num_limpo)
+    return total / len(livros)
+
+def contar_cinco_estrelas(livros):
+    total = 0
+    for livro in livros:
+        nota_limpa: str = livro["nota"].lower().strip()
+        if nota_limpa == "five":
+            total += 1
+    return total
+
+if __name__ == "__main__":
+    livros = ler_livros()
+    # print(f"Quantidade de livros é de: {len(livros)} livros.")
+    # print(f"Preço médio dos livros: {round(calcular_preco_medio(livros), 2)} libras.")
+    # print(f"Quantidade de livros com 5 estrelas: {contar_cinco_estrelas(livros)}")
